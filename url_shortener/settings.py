230fc0d8e3d437233efbc5d0f7cff917f0e408f9
Aleksey Yablunev <yablunev.av@gmail.com>
2023-12-23 01:48:19 +0300

    added dotenv

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -1,4 +1,9 @@
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,10 +13,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-vad=sdr#v422=gs!oa8ls)s_b6)87n0*f=()gd6j^7bz7pm*ds"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
