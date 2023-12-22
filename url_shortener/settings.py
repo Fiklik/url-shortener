b97d49dbfe0f7710a67bc76ec650ff2686642b9f
Aleksey Yablunev <yablunev.av@gmail.com>
2023-12-23 02:00:29 +0300

    fixed styles

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['shortyoururl.onrender.com']
+ALLOWED_HOSTS = ['shortyoururl.onrender.com',  '127.0.0.1']
 
 
 # Application definition
