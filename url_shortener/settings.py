cdcef79dd7263e4be1488f07f663aa2f8dcc31e0
Aleksey Yablunev <yablunev.av@gmail.com>
2023-12-24 14:17:58 +0300

    fixed styles

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -111,12 +111,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-if DEBUG:
-    STATICFILES_DIRS = (
-        BASE_DIR / 'static',
-    )
-else:
-    STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
