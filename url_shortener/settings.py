76f45bd92be4e5926289cfe1177b3cb9abe4e99b
Aleksey Yablunev <yablunev.av@gmail.com>
2023-12-24 21:59:08 +0300

    fixed styles

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -7,6 +7,7 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -116,6 +117,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "url_shortener/static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
